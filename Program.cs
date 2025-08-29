905d2cc6be471797a4e5a2a1a27602fdb1f27ee6
sourabh123456 <sourabhshrm24@gmail.com>
2025-08-29 12:31:37 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@ using System.Text.Unicode;
 Console.WriteLine("Hello, World!");
 
 
+//In C#, encoding refers to the process of converting characters (like letters and symbols) into a sequence of bytes, and vice versa.
+//This is crucial when you're working with text data—especially when saving to files, sending over networks, or interfacing with systems
+//that use different character sets.
+
+//🔤 What Encoding Does
+//Encodes: Converts a string (e.g., "Hello") into bytes.
+
+//Decodes: Converts bytes back into a string.
+
+
 string stringToEncode = "Hello";
 // Encoded Steps
 byte[] strbyte =  Encoding.UTF8.GetBytes(stringToEncode); // Convert string to byte array;
@@ -17,4 +27,5 @@ byte[] bytearrfromBase64string =  Convert.FromBase64String(encodedString);  // C
 string decodedstring = Encoding.UTF8.GetString(bytearrfromBase64string);    // Getstring from byte array.
 
 Console.Read();
+
 
